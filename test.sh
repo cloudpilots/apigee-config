6a186eeabd82cbc6bf17b34eddfc84ff7e22dd5c
Louis Gietzen <louis.gietzen@cloudpilots.com>
2023-03-13 16:39:16 +0100

    WIP test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,16 @@
-for d in */ ; do
-    echo "$d"
-    suffix="/"
-    sf=${d%"$suffix"}
-    jq -s 'reduce inputs as $i (.; .list += $i.list) | flatten' $d/* > "$sf.json"
+gcloud secrets list --project=cpl-apigee-ng --format=json > secrets.json
+SECRETS=$(gcloud secrets list --project=cpl-apigee-ng --format=json)
+jq -c '.[].name' secrets.json | while read i; do
+    # do stuff with $i
+    prefix="projects/329187474656/secrets/"
+    secret=${i#"$prefix"}
+    echo $secret
+    # gcloud secrets versions access latest --secret=$$secret
 done
+# for d in */ ; do
+#     echo "$d"
+#     suffix="/"
+#     sf=${d%"$suffix"}
+#     jq -s 'reduce inputs as $i (.; .list += $i.list) | flatten' $d/* > "$sf.json"
+# done
 
