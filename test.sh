4d878ed06ab9c6672cbb560ff59070dc2da0ff47
Louis Gietzen <louis.gietzen@cloudpilots.com>
2023-05-17 06:54:58 +0200

    Fix merge script

--- test.sh
+++ test.sh
@@ -17,28 +17,28 @@
 # done < ./processed-secrets.json
 
 merge_config() {
+    echo "$(cat .github/outputs/all_changed_files.json)"
     for d in ./config/$1/* ; do
         echo "in the directory: $d"
         for f in $d/* ; do
         prefix="./"
         pf=${f#"$prefix"}
         echo "the processed file: $pf"
-        isInArray=$(cat changed_files.json | jq '. | index("$pf") != null')
-        echo "$(cat changed_files.json)"
+        isInArray=$(jq -e --arg file "$pf" '.|any(. == $file)' .github/outputs/all_changed_files.json)
         echo "isInArray: $isInArray"
-        if [[ $isInArray ]];then
+        if [[ $isInArray == false ]];then
             echo "$pf not in changed files => removing"
-            # rm -f $f
+            rm -f $f
         fi
         done
-        # suffix="/"
-        # sf=${d%"$suffix"}
-        # echo "the processed directory: $sf"
-        # if [[ "$sf" == *"developerApp"* ]];then
-        #     jq -s 'reduce .[] as $item ({}; . * $item)' $d/* > "$sf.json"
-        # else
-        #     jq -s 'reduce inputs as $i (.; .list += $i.list) | flatten' $d/* > "$sf.json"
-        # fi
+        suffix="/"
+        sf=${d%"$suffix"}
+        echo "the processed directory: $sf"
+        if [[ "$sf" == *"developerApp"* ]];then
+            jq -s 'reduce .[] as $item ({}; . * $item)' $d/* > "$sf.json"
+        else
+            jq -s 'reduce inputs as $i (.; .list += $i.list) | flatten' $d/* > "$sf.json"
+        fi
     done
 }
 
