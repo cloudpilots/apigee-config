6e9701fba39d10e171370d599af9a7b788f57a0c
Louis Gietzen <louis.gietzen@cloudpilots.com>
2023-05-17 05:57:15 +0200

    Only merge changes

--- test.sh
+++ test.sh
@@ -18,19 +18,27 @@
 
 merge_config() {
     for d in ./config/$1/* ; do
-        echo "$d"
-        if [[(( $(jq < .github/outputs/all_changed_files.json '[.[].fileName | select(. == "$d")] | length') = 0))]] ;then
-            continue
-        if
-
-        suffix="/"
-        sf=${d%"$suffix"}
-        echo $sf
-        if [[ "$sf" == *"developerApp"* ]];then
-            jq -s 'reduce .[] as $item ({}; . * $item)' $d/* > "$sf.json"
-        else
-            jq -s 'reduce inputs as $i (.; .list += $i.list) | flatten' $d/* > "$sf.json"
+        echo "in the directory: $d"
+        for f in $d/* ; do
+        prefix="./"
+        pf=${f#"$prefix"}
+        echo "the processed file: $pf"
+        isInArray=$(cat changed_files.json | jq '. | index("$pf") != null')
+        echo "$(cat changed_files.json)"
+        echo "isInArray: $isInArray"
+        if [[ $isInArray ]];then
+            echo "$pf not in changed files => removing"
+            # rm -f $f
         fi
+        done
+        # suffix="/"
+        # sf=${d%"$suffix"}
+        # echo "the processed directory: $sf"
+        # if [[ "$sf" == *"developerApp"* ]];then
+        #     jq -s 'reduce .[] as $item ({}; . * $item)' $d/* > "$sf.json"
+        # else
+        #     jq -s 'reduce inputs as $i (.; .list += $i.list) | flatten' $d/* > "$sf.json"
+        # fi
     done
 }
 
